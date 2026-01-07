52c9ec4716e5689771a1eecf8220069d16732e4e
mscoggs <matthewscoggins13@gmail.com>
2026-01-07 14:59:00 -0800

    fixing bug

--- analysis.py
+++ analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 import numpy as np
-from scipy.interpolate import griddata
-from scipy.interpolate import Rbf
+#from scipy.interpolate import griddata
+#from scipy.interpolate import Rbf
 from consts import *
 import glob
 
@@ -24,20 +24,20 @@ def get_cube_avg_flux(dir_):
     df2 = pd.read_csv(cube_avg_filelo, skiprows=9,sep='\s+',names=names)
     v,x,y,z = df["velocity"],df["+x"]+df["-x"]+df2["+x"]+df2["-x"], df["+y"]+df["-y"]+df2["+y"]+df2["-y"], df["+z"]+df2["+z"]
     v,x,y,z = v.values, x.values, y.values, z.values
-    flux = z*l*w  + l*h*(y+z)
+    flux = z*l*w  + x*h*w + y*l*h
 
-    cube_std_filehi = dir_+"/HiDensity/cube_std.txt"
-    cube_std_filelo = dir_+"/LoDensity/cube_std.txt"
-    names = ["velocity", "+x", "-x", "+y", "-y", "+z", "-z", "Earth", "Sun", "anti-Sun", "rot (x)", "rot (y)", "rot (z)"]
-    df = pd.read_csv(cube_std_filehi, skiprows=9,sep='\s+',names=names)
-    df2 = pd.read_csv(cube_std_filelo, skiprows=9,sep='\s+',names=names)
-    A_x_sq,A_y_sq,A_z_sq = (l*h)**2,(w*h)**2,(l*w)**2
-    x_std_sq = (np.power(df["+x"].values,2)  +  np.power(df["-x"].values,2)  +  np.power(df2["+x"].values,2)  +  np.power(df2["-x"].values,2) ) * A_x_sq
-    y_std_sq = (np.power(df["+y"].values,2)  +  np.power(df["-y"].values,2)  +  np.power(df2["+y"].values,2)  +  np.power(df2["-y"].values,2) ) * A_y_sq
-    z_std_sq = (np.power(df["+z"].values,2)  +  np.power(df2["+z"].values,2)  ) * A_z_sq
-    std_sq = x_std_sq+y_std_sq+z_std_sq
+    # cube_std_filehi = dir_+"/HiDensity/cube_std.txt"
+    # cube_std_filelo = dir_+"/LoDensity/cube_std.txt"
+    # names = ["velocity", "+x", "-x", "+y", "-y", "+z", "-z", "Earth", "Sun", "anti-Sun", "rot (x)", "rot (y)", "rot (z)"]
+    # df = pd.read_csv(cube_std_filehi, skiprows=9,sep='\s+',names=names)
+    # df2 = pd.read_csv(cube_std_filelo, skiprows=9,sep='\s+',names=names)
+    # A_x_sq,A_y_sq,A_z_sq = (l*h)**2,(w*h)**2,(l*w)**2
+    # x_std_sq = (np.power(df["+x"].values,2)  +  np.power(df["-x"].values,2)  +  np.power(df2["+x"].values,2)  +  np.power(df2["-x"].values,2) ) * A_x_sq
+    # y_std_sq = (np.power(df["+y"].values,2)  +  np.power(df["-y"].values,2)  +  np.power(df2["+y"].values,2)  +  np.power(df2["-y"].values,2) ) * A_y_sq
+    # z_std_sq = (np.power(df["+z"].values,2)  +  np.power(df2["+z"].values,2)  ) * A_z_sq
+    # std_sq = x_std_sq+y_std_sq+z_std_sq
 
-    return v,flux,std_sq
+    return v,flux#,std_sq
 
 
 
@@ -74,22 +74,24 @@ def pull_phi_theta(fname):
 def consolidate_sims(save_name, data_folder = DATA_FOLDER,vmin = 0, vmax=90):
     data_folders = get_all_sim_dirs(data_folder = data_folder)
 
-
     phis, thetas, flux,stds=[],[],[],[]
     for run_name in data_folders:
         phi, theta = pull_phi_theta(run_name)
-        v,flux_dist,std_sq = get_cube_avg_flux(run_name)
+        #v,flux_dist,std_sq = get_cube_avg_flux(run_name)
+        v,flux_dist = get_cube_avg_flux(run_name)
         args_ = np.where((v>vmin) & (v<vmax))
         avg_flux = np.sum(flux_dist[args_])
-        std = np.sqrt(np.sum(std_sq[args_]))
+        #std = np.sqrt(np.sum(std_sq[args_]))
 
         phis.append(phi)
         thetas.append(theta)
         flux.append(avg_flux)
-        stds.append(std)
-
-    thetas,phis, flux,stds = np.array(thetas),np.array(phis), np.array(flux),np.array(stds)
-    df = pd.DataFrame(data={'theta': thetas, 'phi': phis, 'flux': flux, "std": stds})
+        #stds.append(std)
+    #
+    # thetas,phis, flux,stds = np.array(thetas),np.array(phis), np.array(flux),np.array(stds)
+    # df = pd.DataFrame(data={'theta': thetas, 'phi': phis, 'flux': flux, "std": stds})
+    thetas,phis, flux= np.array(thetas),np.array(phis), np.array(flux)
+    df = pd.DataFrame(data={'theta': thetas, 'phi': phis, 'flux': flux})
     df.to_csv("flux_files/"+save_name)
 
 def read_flux_file(name):
